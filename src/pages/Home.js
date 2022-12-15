a627a3740251259bfb0a1283ffbdb7b9b4d11eb4
profgreatwonder <muoghaluchidinma@gmail.com>
2022-12-15 07:35:23 +0100

    hero image resizing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import {
 	WhiteBgPurchaseChoiceBtn,
 } from "../components/Buttons";
 import Navbar from "../components/Navbar";
+
 import homepage from "../images/heroImages/homepage_hero_bg.png";
 
 const Home = () => {
@@ -18,42 +19,42 @@ const Home = () => {
 
 const Hero = () => {
 	return (
-		<div className="ms-4 my-5 py-5 container">
-			<div className="row">
-				<div className="col-md-6 col-sm-12">
-					<div className="">
-						<p className="text-black mb-0">LOREM IPSUM</p>
+		<div className="ms-4 my-5 py-5">
+			{/* <div className="row"> */}
+			<div className="col-md-6 col-sm-12">
+				<div className="">
+					<p className="text-black mb-0">LOREM IPSUM</p>
 
-						<h1 className="">
-							Fastest <span className="greenText">Delivery</span> and{" "}
-							<span className="greenText">Easy Pickup</span>
-						</h1>
+					<h1 className="">
+						Fastest <span className="greenText">Delivery</span> and{" "}
+						<span className="greenText">Easy Pickup</span>
+					</h1>
 
-						<p className="text-black">
-							Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
-							dolore possimus sequi assumenda nemo? Illo consequuntur aliquam
-							illum vitae porro?
-						</p>
-					</div>
-
-					<div className="d-flex">
-						<div className="me-3">
-							<GreenBgPurchaseChoiceBtn
-								text="Soup/Lunch"
-								link={
-									"https://docs.google.com/forms/d/e/1FAIpQLSd1uQcGEOA2AUZGqdqPcaI3SIG5d9mIL2EOKSXZz1xGCUfwkQ/viewform?usp=sharing"
-								}
-							/>
-						</div>
-						<WhiteBgPurchaseChoiceBtn
-							text="Foodstuff"
-							link={"https://forms.gle/Mvz2DNsXWG5xi6iR8"}
-						/>
-					</div>
+					<p className="text-black">
+						Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex dolore
+						possimus sequi assumenda nemo? Illo consequuntur aliquam illum vitae
+						porro?
+					</p>
 				</div>
 
-				<div className="col-md-6"></div>
+				<div className="d-flex">
+					<div className="me-3">
+						<GreenBgPurchaseChoiceBtn
+							text="Soup/Lunch"
+							link={
+								"https://docs.google.com/forms/d/e/1FAIpQLSd1uQcGEOA2AUZGqdqPcaI3SIG5d9mIL2EOKSXZz1xGCUfwkQ/viewform?usp=sharing"
+							}
+						/>
+					</div>
+					<WhiteBgPurchaseChoiceBtn
+						text="Foodstuff"
+						link={"https://forms.gle/Mvz2DNsXWG5xi6iR8"}
+					/>
+				</div>
 			</div>
+
+			<div className="col-md-6"></div>
+			{/* </div> */}
 		</div>
 	);
 };
